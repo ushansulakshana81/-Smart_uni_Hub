a37348a3b8b2b7691c0b1e1b280e7247672eea3b
Ushan Kariyawasam <ushansulakshana81@users.noreply.github.com>
2026-04-05 10:15:49 +0530

    feat: Initialize client and server structure with Maven and React
    - Added package.json for client with dependencies including React, Axios, and Zustand.
    - Created .gitattributes and .gitignore for server to manage line endings and ignore build artifacts.
    - Introduced Maven wrapper files (mvnw, mvnw.cmd, and properties) for easier Maven management.
    - Added pom.xml for server with Spring Boot dependencies for MongoDB, Security, and Validation.
    - Implemented main application class (SmartCampusHubApplication) and application configuration (application.yaml).
    - Created test class (SmartCampusHubApplicationTests) for basic context loading verification.

---
+++ server/src/main/java/com/sliit/paf/smart_campus_hub/SmartCampusHubApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.paf.smart_campus_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusHubApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/sliit/paf/smart_campus_hub/SmartCampusHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.paf.smart_campus_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
